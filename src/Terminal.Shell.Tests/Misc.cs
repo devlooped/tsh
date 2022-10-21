c9e7e7d1f38d4d11bed8ee8d6ab1c7bb24b1c26a
Daniel Cazzulino <daniel@cazzulino.com>
2022-10-21 15:01:23 -0300

    Test generating DGML from composition

--- src/Terminal.Shell.Tests/Misc.cs
+++ src/Terminal.Shell.Tests/Misc.cs
@@ -1,10 +1,8 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Reflection;
 using System.Text;
 using CliWrap;
-using Microsoft;
-using Microsoft.VisualBasic;
-using Microsoft.VisualStudio.Threading;
-using Terminal.Gui;
+using Microsoft.VisualStudio.Composition;
 using Xunit.Abstractions;
 
 namespace Terminal.Shell;
@@ -15,16 +13,27 @@ public record Misc(ITestOutputHelper Output)
     public async Task TestAsync()
     {
         var output = new StringBuilder();
-        var result = await Cli.Wrap("ghs")
+        var result = await Cli.Wrap("dotnet")
             .WithArguments("--version")
             .WithStandardOutputPipe(PipeTarget.ToStringBuilder(output))
             .WithValidation(CommandResultValidation.None)
             .ExecuteAsync();
-
-
-
+        
         Output.WriteLine(output.ToString());
     }
 
+    [Fact]
+    public void RunDgml()
+    {
+        var discovery = new AttributedPartDiscovery(Resolver.DefaultInstance, true);
+        var catalog = ComposableCatalog.Create(Resolver.DefaultInstance)
+            .AddParts(discovery.CreatePartsAsync(typeof(ShellApp).Assembly).Result)
+            .AddParts(discovery.CreatePartsAsync(Assembly.GetExecutingAssembly()).Result)
+            .WithCompositionService();
+
+        var config = CompositionConfiguration.Create(catalog);
+        var dgml = config.CreateDgml();
+        dgml.Save(@"C:\Delete\composition.dgml");
+    }
 }
 
