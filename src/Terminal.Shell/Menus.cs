dd0334ce44d1504ee4a606bcfcc9e63a1f558608
Daniel Cazzulino <daniel@cazzulino.com>
2022-10-25 18:28:29 -0300

    Rework shell top level menu to be dynamic and exports-based
    Initially, just File.Exit is provided as a proof of concept.

---
+++ src/Terminal.Shell/Menus.cs
@@ -0,0 +1,10 @@
+﻿using System.Composition;
+
+namespace Terminal.Shell;
+
+[Shared]
+partial class Menus
+{
+    [MenuCommand("File._Exit")]
+    internal void Exit() => Application.ExitRunLoopAfterFirstIteration = true;
+}
