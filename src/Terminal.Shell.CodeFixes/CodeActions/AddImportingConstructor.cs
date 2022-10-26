d2ff34dca60ec90815330c73326e2a3896e0c255
Daniel Cazzulino <daniel@cazzulino.com>
2022-10-26 14:40:48 -0300

    Add analyzer and codefix for missing [ImportingConstructor]
    This is another common user error, where you add a constructor and forget to
    add the (however ridiculous for cases where you have only ONE constructor)
    required annotation on it for MEF to pick it up.
    
    Closes #16

---
+++ src/Terminal.Shell.CodeFixes/CodeActions/AddImportingConstructor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Terminal.Shell.CodeActions;
+
+public class AddImportingConstructor : CodeAction
+{
+    readonly Document document;
+    readonly SyntaxNode root;
+    readonly ConstructorDeclarationSyntax declaration;
+
+    public AddImportingConstructor(Document document, SyntaxNode root, ConstructorDeclarationSyntax declaration)
+        => (this.document, this.root, this.declaration)
+        = (document, root, declaration);
+
+    public override string Title => "Add [ImportingConstructor]";
+    public override string EquivalenceKey => Title;
+
+    protected override Task<Document> GetChangedDocumentAsync(CancellationToken cancellationToken)
+        => Task.FromResult(document.WithSyntaxRoot(
+            root.ReplaceNode(declaration,
+                declaration.AddAttributeLists(
+                    AttributeList(
+                        SingletonSeparatedList(
+                            Attribute(
+                                IdentifierName("ImportingConstructor"))))))));
+}
