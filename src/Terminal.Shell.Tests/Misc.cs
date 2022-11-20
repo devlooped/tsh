c701b03c529d59258703547c578268aeceb7ad04
Daniel Cazzulino <daniel@cazzulino.com>
2022-11-20 20:01:21 -0300

    Spike context expression evaluation

--- src/Terminal.Shell.Tests/Misc.cs
+++ src/Terminal.Shell.Tests/Misc.cs
@@ -1,7 +1,9 @@
-﻿using System.Composition;
+﻿using System.ComponentModel;
 using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using CliWrap;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.VisualStudio.Composition;
 using Scriban;
 using SharpYaml.Serialization;
@@ -24,8 +26,108 @@ public partial class Foo : IFoo, IBar
 
 }
 
+public interface IExpressionContext
+{
+    public HashSet<string>? Data { get; set; }
+}
+
+public static partial class ExpressionContext
+{
+    public static partial class Generated
+    {
+        [Export(typeof(IExpressionContext))]
+        [ExportMetadata("Expression", "Initialized && GitHub && !GitHubUser")]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public partial class _e98d5c36e753a133520419cd954dde3e : IExpressionContext
+        {
+            public bool Initialized => Data?.Contains(nameof(Initialized)) == true;
+            public bool GitHub => Data?.Contains(nameof(GitHub)) == true;
+            public bool GitHubUser => Data?.Contains(nameof(GitHubUser)) == true;
+
+            public HashSet<string>? Data { get; set; }
+        }
+    }
+}
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class ContextExpression
+{
+    public static object CreateContext(string expression, HashSet<string> context) => expression switch  
+    {
+        "Initialized && GitHub && !GitHubUser" => new GeneratedContexts._e98d5c36e753a133520419cd954dde3e(context),
+        _ => throw new NotImplementedException(),
+    };
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class GeneratedContexts
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public class _e98d5c36e753a133520419cd954dde3e
+        {
+            readonly HashSet<string> context;
+            public _e98d5c36e753a133520419cd954dde3e(HashSet<string> context) => this.context = context;
+
+            public bool Initialized => context.Contains(nameof(Initialized));
+            public bool GitHub => context.Contains(nameof(GitHub));
+            public bool GitHubUser => context.Contains(nameof(GitHubUser));
+        }
+    }
+}
+
+//public class FooCommandHandler : ICommandHandler<Foo>
+//{
+//    public bool CanExecute(Foo command) => Validator.TryValidateObject(command, new ValidationContext(command), null, true);
+    
+//    public void Execute(Foo command) => throw new NotImplementedException();
+//}
+
 public record Misc(ITestOutputHelper Output)
 {
+    [Fact]
+    public async Task GetContextFromExport()
+    {
+        var expression = "Initialized && GitHub && !GitHubUser";
+        var provider = CompositionSetup.CreateDefaultProvider();
+        var contexts = provider.GetExports<IExpressionContext, IDictionary<string, object?>>();
+        var context = contexts.First(x => 
+            x.Metadata.TryGetValue("Expression", out var value) && 
+            value is string metadata && 
+            metadata == "Initialized && GitHub && !GitHubUser").Value;
+
+        Assert.NotNull(context);
+
+        var script = CSharpScript.Create<bool>(expression, globalsType: context.GetType());        
+        var runner = script.CreateDelegate();
+
+        var data = new HashSet<string>(new[] { "Initialized", "GitHub", "GitHubUser" });
+        context.Data = data;
+
+        Assert.False(await runner(context));
+        data.Remove("GitHubUser");
+        Assert.True(await runner(context));
+    }
+
+    [Fact]
+    public async Task EvaluateExpression()
+    {
+        var expression = "Initialized && GitHub && !GitHubUser";
+        
+        // Gets a hash for the expression, to use as the map between expression > generated code.
+        var hash = string.Concat(MD5.HashData(Encoding.UTF8.GetBytes(expression)).Select(b => b.ToString("x2")));
+        Output.WriteLine(hash);
+
+        var context = new HashSet<string>(new[] { "Initialized", "GitHub", "GitHubUser" });
+        var evaluation = Terminal.Shell.ContextExpression.CreateContext(expression, context);
+        
+        var typed = CSharpScript.Create<bool>(expression, globalsType: evaluation.GetType());
+        var name = typed.GetCompilation().AssemblyName;
+        var runner = typed.CreateDelegate();
+
+        Assert.False(await runner(evaluation));
+        context.Remove("GitHubUser");
+        Assert.True(await runner(evaluation));
+    }
+
     [Fact]
     public async Task TestAsync()
     {
