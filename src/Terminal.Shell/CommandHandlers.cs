e5698079167b218d141d1742d11fbabd46a4cfbd
Daniel Cazzulino <daniel@cazzulino.com>
2022-11-26 20:02:35 -0300

    Add first shell event and status bar updating

---
+++ src/Terminal.Shell/CommandHandlers.cs
@@ -0,0 +1,26 @@
+﻿namespace Terminal.Shell;
+
+[Shared]
+partial class SetStatusHandler : ICommandHandler<SetStatus>
+{
+    readonly Lazy<ShellApp?> shell;
+    readonly IThreadingContext threading;
+
+    [ImportingConstructor]
+    public SetStatusHandler(Lazy<ShellApp?> shell, IThreadingContext threading)
+    {
+        this.shell = shell;
+        this.threading = threading;
+    }
+
+    public bool CanExecute(SetStatus command) => true;
+
+    public void Execute(SetStatus command)
+    {
+        if (shell.Value?.StatusBar is StatusBar status)
+        {
+            status.RemoveItem(0);
+            status.AddItemAt(0, new StatusItem(Key.Null, command.Status, () => { }));
+        }
+    }
+}
